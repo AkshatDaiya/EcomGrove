5ae3c0aa3e5a61133ad4982ceb313322d307b8fd
Akshat <daiyaakshat4@gmail.com>
2024-02-22 15:16:43 +0530

    add cors link

--- backEnd/index.js
+++ backEnd/index.js
@@ -10,7 +10,7 @@ mongoose.connect(`${process.env.DB_URL}`)
 const Router = require('./routers/api');
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://grull-task.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
