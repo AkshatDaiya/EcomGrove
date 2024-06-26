58cbc874d1045b63db639faa7414a815743ebfc2
Akshat <daiyaakshat4@gmail.com>
2024-06-26 17:35:53 +0530

    did some improvements

--- front_end/src/Reg.js
+++ front_end/src/Reg.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Reg() {
   const [username, setUsername] = useState("");
@@ -8,56 +8,111 @@ function Reg() {
   const [password, setPassword] = useState("");
   const [isBtnClicked, setIsBtnClicked] = useState(false);
   const [message, setMessage] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const regRecord = { username, email, password }
-    axios.post('https://grull-task-aprk.vercel.app/api/reg', { regRecord })
+    e.preventDefault();
+    setIsBtnClicked(true);
+
+    const regRecord = { username, email, password };
+    axios
+      .post("https://grull-task-aprk.vercel.app/api/reg", { regRecord })
       .then((response) => {
         if (response.data.status === 201) {
-          navigate('/')
-          setIsBtnClicked(false)
+          navigate("/");
+          setIsBtnClicked(false);
         }
       })
-      .catch((error) => { setMessage(error.response.data.message) })
-    setIsBtnClicked(true)
-  }
+      .catch((error) => {
+        setMessage(error.response.data.message);
+        setIsBtnClicked(false);
+      });
+  };
 
   return (
     <div>
-      <div className="row w-100 m-0 MainRegPage" style={{ height: "37.96rem" }}>
+      <div
+        className="row w-100 m-0 MainRegPage"
+        style={{ height: "37.96rem" }}
+      >
         <div className="col-md-5 reg">
           <div className="logo">
             <h4>myLOGO</h4>
           </div>
-          <form className='regForm' onSubmit={handleSubmit}>
-            <h3 className='mb-5'>Register your account</h3>
+          <form
+            className="regForm"
+            onSubmit={handleSubmit}
+          >
+            <h3 className="mb-5">Register your account</h3>
 
-            <h5 className='text-danger text-center'>{message}</h5>
+            <h5 className="text-danger text-center">{message}</h5>
 
             <label htmlFor="">Full Name</label>
-            <input type="text" className='form-control mb-3' value={username} onChange={(e) => { setUsername(e.target.value) }} autoComplete='username' required />
+            <input
+              type="text"
+              className="form-control mb-3"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              autoComplete="username"
+              required
+            />
 
             <label htmlFor="">Email Address</label>
-            <input type="email" className='form-control mb-3' value={email} onChange={(e) => { setEmail(e.target.value) }} autoComplete='email' required />
+            <input
+              type="email"
+              className="form-control mb-3"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              autoComplete="email"
+              required
+            />
 
             <label htmlFor="">Password</label>
-            <input type="password" className='form-control mb-4' value={password} onChange={(e) => { setPassword(e.target.value) }} autoComplete='current-password' required />
+            <input
+              type="password"
+              className="form-control mb-4"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              autoComplete="current-password"
+              required
+            />
 
-            <button type="submit" className='form-control'>{isBtnClicked ? (<img src='./Rolling.gif' alt=''></img>) : "Register now"}</button>
+            <button
+              type="submit"
+              className="form-control"
+            >
+              {isBtnClicked ? (
+                <img
+                  src="./Rolling.gif"
+                  alt=""
+                ></img>
+              ) : (
+                "Register now"
+              )}
+            </button>
           </form>
           <hr />
-          <h6 className='text-center'>Already have an account?</h6>
+          <h6 className="text-center">Already have an account?</h6>
           <hr />
-          <Link to={'/login'}>
-            <button type="submit" className='form-control btn btn-outline-warning'>LOGIN</button>
+          <Link to={"/login"}>
+            <button
+              type="submit"
+              className="form-control btn btn-outline-warning"
+            >
+              LOGIN
+            </button>
           </Link>
         </div>
         <div className="col-md-7 image"></div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Reg
+export default Reg;
